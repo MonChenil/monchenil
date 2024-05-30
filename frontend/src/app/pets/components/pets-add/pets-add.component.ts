2a6c5c0de2116d1e2942aea8dab39aeecc90dcd0
Samy MOSA <samymosa02@gmail.com>
2024-05-30 15:10:05 +0200

    Update Pets to match the new api

--- frontend/src/app/pets/components/pets-add/pets-add.component.ts
+++ frontend/src/app/pets/components/pets-add/pets-add.component.ts
@@ -15,29 +15,28 @@ export class PetsAddComponent {
 
   @Output() petAdded = new EventEmitter();
 
-  petTypes = ['Chat', 'Chien']; // Order is important. Must match backend/MonChenil/Entities/Pet.cs
+  petTypes = ['Chien', 'Chat']; // Order is important. Must match backend/src/MonChenil.Domain/Pets/PetType.cs
 
   form = this.formBuilder.group({
+    id: ['', [Validators.required, Validators.pattern(/^\d{15}$/)]],
     name: ['', [Validators.required, Validators.minLength(3)]],
     type: ['', Validators.required],
-    incompatibleTypes: this.formBuilder.array(
-      this.petTypes.map(() => this.formBuilder.control(false)),
-    ),
   });
 
   onSubmit() {
-    const selectedIncompatibleTypes: number[] =
-      this.form.value.incompatibleTypes
-        ?.map((checked, index) => (checked ? index : -1))
-        .filter((index) => index !== -1) || [];
+    if (this.form.invalid) {
+      return;
+    }
 
     const petToAdd = {
-      name: this.form.value.name,
+      id: {
+        value: this.form.value.id!,
+      },
+      name: this.form.value.name!,
       type: parseInt(this.form.value.type!),
-      incompatibleTypes: selectedIncompatibleTypes,
     };
 
-    this.petsService.create(petToAdd).subscribe({
+    this.petsService.createPet(petToAdd).subscribe({
       next: (response) => {
         this.petAdded.emit(response);
         this.form.reset({
@@ -63,6 +62,10 @@ export class PetsAddComponent {
 
     if (field.errors['minlength']) {
       return `Ce champ doit contenir au moins ${field.errors['minlength'].requiredLength} caractères`;
+    }
+
+    if (field.errors['pattern']) {
+      return 'Ce champ doit contenir 15 chiffres';
     }
 
     if (field.errors['httpError']) {
