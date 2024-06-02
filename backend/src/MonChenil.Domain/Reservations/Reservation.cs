936afd17a59ffc1160a5f342c99b91179f62b7c5
Samy MOSA <samymosa02@gmail.com>
2024-06-02 13:11:49 +0200

    fix: Fix start date tests

--- backend/src/MonChenil.Domain/Reservations/Reservation.cs
+++ backend/src/MonChenil.Domain/Reservations/Reservation.cs
@@ -14,13 +14,6 @@ public class Reservation
     {
         Id = id;
         OwnerId = ownerId;
-        if (DateTime.Now.Subtract(startDate).TotalHours < 1)
-        {
-            throw new ReservationStartDateException();
-        } else StartDate = startDate;
-        if(startDate.AddDays(1).CompareTo(endDate)>0) {
-            throw new ArgumentException("Au moins un jour");
-        } else EndDate = endDate;
     }
 
     public void AddPets(IEnumerable<Pet> pets)
---
+++ backend/tests/MonChenil.Api.Tests/Requests/CreateReservationRequestTests.cs
@@ -0,0 +1,33 @@
+﻿using MonChenil.Api.Requests;
+using MonChenil.Domain.Pets;
+using MonChenil.Domain.Reservations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonChenil.Api.Tests.Requests
+{
+    public class CreateReservationRequestTests
+    {
+        [Fact]
+        public void StartDate_LessThanOneHourFromNow_ThrowsReservationStartDateException()
+        {
+            var startDate = DateTime.Now.AddMinutes(30);
+            var endDate = DateTime.Now.AddDays(1);
+            List<PetId> petIds = [];
+            Assert.Throws<ReservationStartDateException>(() => new CreateReservationRequest(startDate, endDate, petIds));
+        }
+
+        [Fact]
+        public void StartDate_MoreThanOneHourFromNow_CreatesReservation()
+        {
+            var startDate = DateTime.Now.AddHours(1).AddMinutes(1);
+            var endDate = DateTime.Now.AddDays(1);
+            List<PetId> petIds = [];
+            var reservation = new CreateReservationRequest(startDate, endDate, petIds);
+            Assert.Equal(startDate, reservation.StartDate);
+        }
+    }
+}
